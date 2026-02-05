052b7e6603245e4fdb6edba71d303a7ba1d9bab0
Nikita Rozanov <roznik20@Nikitas-MacBook-Pro-1130.local>
2026-02-04 20:32:15 -0500

    new lesson

--- Java fundamentals/src/HellowWorld.java
+++ Java fundamentals/src/HellowWorld.java
@@ -3,9 +3,9 @@ public class HellowWorld {
     public static void main(String[] args) {
         String greetings;
         greetings = " greetings to you!";
-        int x = 11;
-        x +=4;
-        x = x - 6;
+        int x = luckyNumberSubstract(22);
+        x = Math.abs(x);
+        System.out.println(x);
         while (x != 12) {
             /* multi
             line 
@@ -23,7 +23,37 @@ public class HellowWorld {
                 ammount = "lucky number of";
             }
             System.out.println(ammount + greetings);
-            System.out.println(x);
         }
+
+        for (int i = 0; i<=12; i++) {
+            int counts;
+            counts = 12 - i;
+            if ( i != 12){
+                System.out.println(counts + " till lucky number");
+            }
+            else{ 
+                System.out.println("here is the lucky numer: " + i);
+            }
+        }
+        String a = "blob";
+        String b = "Blob";
+        boolean nonCapital = a.equals(b);
+        boolean capital = a.equalsIgnoreCase(b);
+        System.out.println(nonCapital);
+        System.out.println(capital);
+        System.out.println(a.length());
+        System.out.println(a.charAt(2));
+
+
+        String numStr = "32";
+        String doubStr = "3.14";
+        int num = Integer.parseInt(numStr);
+        double doub = Double.parseDouble(doubStr);
+        double sum = num + doub;
+        System.out.println(sum);
+    }
+    public static int luckyNumberSubstract(int num) {
+        int luckyNumSub = 12 - num;
+        return luckyNumSub;
     }
 }
---
+++ Java fundamentals/src/arrTrial.java
@@ -0,0 +1,6 @@
+public class arrTrial{
+    public static void main (){
+        String[] daybreak = {"monday","wensder","tuesday wensday duality"};
+        System.out.println(daybreak[2]);
+    }
+}
